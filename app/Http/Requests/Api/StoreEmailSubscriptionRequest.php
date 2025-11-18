274a297888b7a745e4740655e9f6152c6cdd5488
mathias-tad <mathiasstadesse@gmail.com>
2025-11-18 12:30:27 +0300

    fix: dns problem

--- app/Http/Requests/Api/StoreEmailSubscriptionRequest.php
+++ app/Http/Requests/Api/StoreEmailSubscriptionRequest.php
@@ -38,7 +38,7 @@ class StoreEmailSubscriptionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'string', 'email:rfc,dns', 'max:255'],
+            'email' => ['required', 'string', 'email:rfc', 'max:255'],
             'name' => ['nullable', 'string', 'max:255'],
             'wants_news' => ['sometimes', 'boolean'],
             'wants_events' => ['sometimes', 'boolean'],
